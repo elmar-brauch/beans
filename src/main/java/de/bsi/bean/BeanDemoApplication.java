fd6c8fca63601209daefbe63fcf0a70e62781872
el_bu <el_bu@LAPTOP-N25AL9L7.mshome.net>
2020-10-18 09:31:27 +0200

    Bean definitions in xml

--- src/main/java/de/bsi/bean/BeanDemoApplication.java
+++ src/main/java/de/bsi/bean/BeanDemoApplication.java
@@ -3,10 +3,15 @@ package de.bsi.bean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 import de.bsi.bean.service.ItemService;
 
 @SpringBootApplication
+// Uncomment the next 2 lines, if you want to use bean definition in xml.
+//@ComponentScan(useDefaultFilters = false)
+//@ImportResource(locations = {"classpath:spring.xml"})
 public class BeanDemoApplication {
 
 	public static void main(String[] args) {
